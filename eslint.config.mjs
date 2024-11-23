7f1b6e6159cb9de6b8d574333cb00bf837cffd4a
Furqan Ansari <sm.furqankhan@gmail.com>
2024-11-23 20:39:38 +0500

    Eslint react rules added.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,22 +3,37 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
 
 export default tseslint.config(
   { ignores: ["dist"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["src/**/*.{ts,tsx}"],
+    // files: ["src/**/*.{ts,tsx}"], // Target TypeScript files
     languageOptions: {
       ecmaVersion: 2020,
+      sourceType: "module",
       globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true, // Enable JSX
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: "detect", // Automatically detects React version
+      },
     },
     plugins: {
+      react: react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
+      ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off", // Disable the rule
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
