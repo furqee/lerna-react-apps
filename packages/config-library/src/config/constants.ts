503b9eba9525438992c4ab66ccd715b8464538df
Furqan Ansari <sm.furqankhan@gmail.com>
2024-12-04 00:10:10 +0500

    config library added.

---
+++ packages/config-library/src/config/constants.ts
@@ -0,0 +1,7 @@
+export const authTokenKey = "authToken";
+
+export const constants = {
+  authTokenKey,
+};
+
+export default constants;
---
+++ packages/config-library/vite.config.ts
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-nodejs-modules */
+import * as path from "path";
+import { getBaseConfig } from "../../vite.config";
+import dts from "vite-plugin-dts";
+
+export default getBaseConfig({
+  lib: {
+    entry: path.resolve(__dirname, "src/index.ts"),
+    name: "ConfigLibrary",
+    fileName: "config-library",
+  },
+  plugins: [dts({ tsconfigPath: "./tsconfig.json", outDir: "dist/types" })],
+});
